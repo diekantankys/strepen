da3d8745ac40d27baafd8dc6f920d7f01341ae2f
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2026-06-26 09:15:49 +0200

    Hide games link on guest auth

--- server/resources/views/layouts/navbar.blade.php
+++ server/resources/views/layouts/navbar.blade.php
@@ -96,7 +96,6 @@
                 @endif
             @else
                 <div class="navbar-end">
-                    <a @class(['navbar-item', 'is-active' => Route::is('games.*')]) href="{{ route('games.index') }}">@lang('layout.navbar.games')</a>
                     <a @class(['navbar-item', 'is-active' => Route::is('apps')]) href="{{ route('apps') }}" wire:navigate>
                         <svg xmlns="http://www.w3.org/2000/svg" class="is-hidden-touch" style="width: 1.5rem; height: 1.5rem;" viewBox="0 0 24 24">
                             <path d="M17,1H7A2,2 0 0,0 5,3V21A2,2 0 0,0 7,23H17A2,2 0 0,0 19,21V3A2,2 0 0,0 17,1M17,19H7V5H17V19M16,13H13V8H11V13H8L12,17L16,13Z" />
