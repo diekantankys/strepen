994f1925405859bb2229f27e3a2db2adb8a28300
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2026-06-28 16:20:32 +0200

    Add soft deletes to game tables

--- server/app/Models/GameAsset.php
+++ server/app/Models/GameAsset.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class GameAsset extends Model
 {
+    use SoftDeletes;
+
+    protected $hidden = ['deleted_at'];
+
     protected $fillable = ['game_id', 'name', 'image_path'];
 
     public function game(): BelongsTo
